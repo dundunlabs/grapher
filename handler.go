ec8315eac8d47770533700d5b8f178e82b274461
tdd28 <tdd2810@gmail.com>
2026-07-14 18:04:19 +0700

    feat: graphql handler

---
+++ handler.go
@@ -0,0 +1,50 @@
+package grapher
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/graph-gophers/graphql-go"
+)
+
+type Handler struct {
+	Schema *graphql.Schema
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.handleGET(w, r)
+	case http.MethodPost:
+		h.handlePOST(w, r)
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *Handler) handleGET(w http.ResponseWriter, r *http.Request) {
+	// Handle GET requests for GraphQL queries
+	// You can implement your logic here to process the query parameters and execute the GraphQL query
+}
+
+func (h *Handler) handlePOST(w http.ResponseWriter, r *http.Request) {
+	var params graphqlParams
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result := h.Schema.Exec(r.Context(), params.Query, params.OperationName, params.Variables)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+type graphqlParams struct {
+	Query         string         `json:"query"`
+	OperationName string         `json:"operationName"`
+	Variables     map[string]any `json:"variables"`
+}
